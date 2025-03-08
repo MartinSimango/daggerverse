6c9413ba54c0ca6125a4ac1f5c1f202e35c780a1
Martin Simango <shukomango@gmail.com>
2025-03-08 13:11:15 +0200

    fix: remove forcing tag signs

--- gopkg/release.go
+++ gopkg/release.go
@@ -48,13 +48,13 @@ func (m *Gopkg) release(
 			// WithExec([]string{"bash", "-c", "echo \"$PASS\" | gpg --import --batch --yes --passphrase-fd 0 /tmp/gpg.key"}).
 			WithExec([]string{"bash", "-c", "echo \"$GPG_KEY\" | gpg --import"}).
 			WithExec([]string{"bash", "-c", "git config commit.gpgsign true"}).
-			WithExec([]string{"bash", "-c", "git config tag.gpgsign true"}).
+			// WithExec([]string{"bash", "-c", "git config tag.gpgsign true"}).
 			// WithExec([]string{"bash", "-c", "git config --global user.name \"Martin Simango\""}).
 			// WithExec([]string{"bash", "-c", "git config --global user.email \"shukomango@gmail.com\""}).
 			WithExec([]string{"bash", "-c", "printf \"trust\n5\ny\nquit\n\" | gpg --batch --command-fd 0 --edit-key 60BEEE74E301083F"}).
 			WithExec([]string{"bash", "-c", fmt.Sprintf("git config --global user.signingkey %s", "60BEEE74E301083F")}).
-			WithExec([]string{"bash", "-c", "git config --global gpg.program gpg"}).
-			WithExec([]string{"bash","-c", "git config --global core.editor \"true\""})
+			WithExec([]string{"bash", "-c", "git config --global gpg.program gpg"})
+			// WithExec([]string{"bash","-c", "git config --global core.editor \"true\""})
 		// WithExec([]string{"bash", "-c", "git commit -a -m \"Test\""})
 	}
 
