a4810746e96c9992bab438408bd8f5ab02fc66fc
Martin Simango <shukomango@gmail.com>
2025-03-08 13:05:17 +0200

    refrator: refactor some code

--- gopkg/release.go
+++ gopkg/release.go
@@ -22,7 +22,7 @@ func (m *Gopkg) release(
 	//		WithExec([]string{"bash", "-c", "gpg-agent --daemon"}).
 
 	container := dag.Container().
-		From("node:23.7.0").
+		From("node:23.7.0-alpine").
 		WithDirectory("/src", source).
 		WithWorkdir("/src").
 		WithMountedCache("/root/.npm", dag.CacheVolume("node-23")).
@@ -35,15 +35,15 @@ func (m *Gopkg) release(
 	if gpgKey != nil {
 		container = container.
 			WithSecretVariable("GPG_KEY", gpgKey).
-			WithEnvVariable("GIT_EMAIL", "shukomango@gmail.com").
-			WithEnvVariable("GIT_USERNAME", "Martin Simango").
-			WithEnvVariable("GIT_AUTHOR_NAME", "Martin Simango").
+			// WithEnvVariable("GIT_EMAIL", "shukomango@gmail.com").
+			// WithEnvVariable("GIT_USERNAME", "Martin Simango").
+			WithEnvVariable("GIT_AUTHOR_NAME", "semantic-release-bot").
 			WithEnvVariable("GIT_AUTHOR_EMAIL", "shukomango@gmail.com").
-			WithEnvVariable("GIT_COMMITTER_NAME", "Martin Simango").
+			WithEnvVariable("GIT_COMMITTER_NAME", "semantic-release-bot").
 			WithEnvVariable("GIT_COMMITTER_EMAIL", "shukomango@gmail.com").
-			WithExec([]string{"apt-get", "update"}).
+			WithExec([]string{"apk", "add", "gnupg","bash","git"}).
+			WithExec([]string{"bash", "-c", "gpg-agent --daemon"}).
 			// WithSecretVariable("PASS", pass).
-			WithExec([]string{"apt-get", "install", "-y", "gnupg", "vim"}).
 			// WithExec([]string{"bash", "-c", "echo \"$GPG_KEY\" > /tmp/gpg.key"}).
 			// WithExec([]string{"bash", "-c", "echo \"$PASS\" | gpg --import --batch --yes --passphrase-fd 0 /tmp/gpg.key"}).
 			WithExec([]string{"bash", "-c", "echo \"$GPG_KEY\" | gpg --import"}).
@@ -53,7 +53,8 @@ func (m *Gopkg) release(
 			// WithExec([]string{"bash", "-c", "git config --global user.email \"shukomango@gmail.com\""}).
 			WithExec([]string{"bash", "-c", "printf \"trust\n5\ny\nquit\n\" | gpg --batch --command-fd 0 --edit-key 60BEEE74E301083F"}).
 			WithExec([]string{"bash", "-c", fmt.Sprintf("git config --global user.signingkey %s", "60BEEE74E301083F")}).
-			WithExec([]string{"bash", "-c", "git config --global gpg.program gpg"})
+			WithExec([]string{"bash", "-c", "git config --global gpg.program gpg"}).
+			WithExec([]string{"bash","-c", "git config --global core.editor \"true\""})
 		// WithExec([]string{"bash", "-c", "git commit -a -m \"Test\""})
 	}
 
